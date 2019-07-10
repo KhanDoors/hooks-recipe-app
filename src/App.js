c68873a4f22d78d2dd0d96d848d01e2e9edfeae2
syyad khan <syyadkhan@yahoo.com>
2019-07-10 11:11:14 -0400

    try dotenv

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-const dotenv = require("dotenv");
 
-dotenv.config();
+require("dotenv").config();
 
-const app_id = process.env.app_id;
-const app_key = process.env.app_key;
+const app_id = process.env.REACT_APP_APP_ID;
+const app_key = process.env.REACT_APP_APP_KEY;
 
 const App = () => {
   const APP_ID = app_id;
   const APP_KEY = app_key;
 
-  const request = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [something, setSomething] = useState();
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <h1>Let's go!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
